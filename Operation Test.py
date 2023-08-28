740f722b29861c563f2b03ce65e92b57877387eb
Abdurrahman Ansari <abdurrahmanansari03@gmail.com>
2023-08-28 10:01:52 +0530

    Create Operation Test
    Created a program to test various operations in python programs.

---
+++ Operation Test.py
@@ -0,0 +1,19 @@
+Num1=int(input(' Enter first number: '))
+print(' First number is',Num1)
+Num2=int(input(' Enter second number: '))
+print(' Second number is',Num2)
+Addition = Num1 + Num2
+Subtration = Num1 - Num2
+Multiplication = Num1 * Num2
+Division = Num1 / Num2
+Modulo = Num1 % Num2
+Floor_Division = Num1 // Num2
+Power = Num1 ** Num2
+
+print('\n Sum of the numbers is:',Addition)
+print('\n Subtration of the number is:',Subtration)
+print('\n Multiplication of the Number: is',Multiplication)
+print('\n Division of the number is:',Division)
+print('\n Modulo of the number is:',Modulo)
+print('\n Floor Division of the number is:',Floor_Division)
+print('\n Division of the number is:',Power)
